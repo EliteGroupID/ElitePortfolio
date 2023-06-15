91808a139fac31936a72ce7f6136bb6ab64b0ef0
sluxz3r <iniarieazhari@gmail.com>
2023-06-15 12:38:25 +0700

    Init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='h-4 bg-red-300'>asd</div>
+    </div>
+  );
+}
+
+export default App;
