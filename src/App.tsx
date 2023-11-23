d0cedc6cdd722d4a6c220266a803681b025a22f3
Arie Azhari <arie.azhari@medirecords.com>
2023-11-24 00:30:20 +0700

    Fix: change slogan

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import { useRef } from "react";
 import EGIcon from "./eg-icon.png";
 import { autoScroll } from "./utils/useScroll";
+import {
+  companyDomainText,
+  companyNameText,
+  sloganDescriptionText,
+  sloganText,
+} from "./constants";
 
 const App = () => {
   const nextSectionRef = useRef<HTMLElement | null>(null);
@@ -17,17 +23,16 @@ const App = () => {
         <div className="w-full text-center px-6 lg:px-12 xl:px-28 flex flex-col justify-center items-center">
           <img src={EGIcon} className="w-28" />
           <div className="mt-4 mb-12 font-semibold text-xl tracking-wider flex">
-            <p>ELiteGroup</p>
+            <p>{companyNameText}</p>
             <span className="px-1 text-black bg-neutral-100 rounded-sm">
-              Tech
+              {companyDomainText}
             </span>
           </div>
           <h1 className="font-semibold text-3xl lg:text-5xl xl:text-7xl tracking-widest">
-            Your Vision, Our Expertise
+            {sloganText}
           </h1>
           <p className="mt-4 text-neutral-300 sm:w-2/3 lg:text-xl xl:text-2xl xl:w-1/2 tracking-wider">
-            High-Quality App Creation and Software Product Development Services
-            for Your Business
+            {sloganDescriptionText}
           </p>
         </div>
         <div className="absolute bottom-8 left-0 right-0 flex justify-center">
---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const companyNameText = 'ELiteGroup'
+export const companyDomainText = 'Tech'
+export const sloganText = 'Your Vision, Our Expertise'
+export const sloganDescriptionText = ' High-Quality Software Development Services and Product for Your Business'
